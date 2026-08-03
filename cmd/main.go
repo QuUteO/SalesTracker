e43ebc8fa7d829f5935290ad333d4dca404fd0aa
QuUteO <quuteo86@gmail.com>
2026-08-03 11:50:05 +0300

    add: config, postgres pool

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"SalesTracker/internal/config"
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	pgxdriver "github.com/wb-go/wbf/dbpg/pgx-driver"
+	"github.com/wb-go/wbf/logger"
+)
+
+func main() {
+	cfg, err := config.New("./config.yaml")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка загрузки конфигурации: %v\n", err)
+		os.Exit(1)
+	}
+
+	log, err := logger.InitLogger(
+		logger.ZapEngine,
+		"SalesTracker",
+		cfg.Env,
+		logger.WithLevel(logger.InfoLevel),
+		logger.WithRotation("logs/app.log", 100, 5, 30),
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Ошибка инициализации логгера: %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Info("Запуск приложения SalesTracker...")
+
+	pg, err := pgxdriver.New(
+		cfg.DSN,
+		log,
+		pgxdriver.MaxPoolSize(50),
+		pgxdriver.MaxConnAttempts(5),
+		pgxdriver.BaseRetryDelay(100*time.Millisecond),
+	)
+	if err != nil {
+		log.Error("Не удалось подключиться к PostgreSQL", "error", err)
+		os.Exit(1)
+	}
+	defer pg.Close()
+
+	ctx := context.Background()
+	if err := pg.Ping(ctx); err != nil {
+		log.Error("PostgreSQL недоступен", "error", err)
+		os.Exit(1)
+	}
+	log.Info("PostgreSQL успешно подключен")
+}
---
+++ internal/config/config.go
@@ -0,0 +1,22 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/wb-go/wbf/config/cleanenv-port"
+)
+
+type Config struct {
+	Env  string `yaml:"env"`
+	DSN  string `yaml:"dsn"`
+	Addr string `yaml:"addr"`
+}
+
+func New(path string) (*Config, error) {
+	var cfg Config
+	if err := cleanenvport.LoadPath(path, &cfg); err != nil {
+		return nil, fmt.Errorf("load config: %w", err)
+	}
+
+	return &cfg, nil
+}
