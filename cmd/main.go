6cf78a078b55b82e62c3719e4490a33b36e5cf16
QuUteO <quuteo86@gmail.com>
2026-08-04 10:25:29 +0300

    add: http server with graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"SalesTracker/internal/config"
+	"SalesTracker/internal/handler"
 	"SalesTracker/internal/repository"
 	"SalesTracker/internal/service"
-	"context"
-	"fmt"
-	"os"
-	"time"
 
 	pgxdriver "github.com/wb-go/wbf/dbpg/pgx-driver"
 	"github.com/wb-go/wbf/logger"
@@ -56,4 +62,37 @@ func main() {
 
 	repo := repository.New(pg)
 	srv := service.New(repo)
+	h := handler.New(srv)
+
+	router := h.InitRoutes(cfg.Env)
+
+	server := &http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		log.Info(fmt.Sprintf("HTTP-сервер запущен на %s", cfg.Addr))
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Error("Критическая ошибка HTTP-сервера", "error", err)
+			os.Exit(1)
+		}
+	}()
+
+	sig := <-stop
+	log.Info("Получен сигнал завершения", "signal", sig.String())
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Error("Принудительная остановка сервера по таймауту", "error", err)
+	} else {
+		log.Info("HTTP-сервер успешно остановлен")
+	}
+
+	log.Info("Приложение SalesTracker успешно завершило работу")
 }
